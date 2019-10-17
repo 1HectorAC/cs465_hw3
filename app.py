7143eadab574ee260ecce7ba7e3302981a085018
Hector Acosta <hacostacervantes17@mail.wou.edu>
2019-10-17 23:28:53 +0000

    Added implementation of endpoint to get all activities from the activity log.

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify
+from datetime import datetime
 app = Flask(__name__)
+
+activity_log = [
+    {
+        'id': 0,
+        'user_id': 1,
+        'username': 'john',
+        'timestamp': datetime.utcnow(),
+        'details': "Important stuff here",
+    },
+    {
+        'id': 1,
+        'user_id': 2,
+        'username': 'yoko',
+        'timestamp': datetime.utcnow(),
+        'details': "Even more important",
+    },
+]
+
+@app.route('/api/activities', methods=["GET"])
+def activities():
+    return jsonify({'activity_log': activity_log})
 
 @app.route('/')
 def hello():
     return "hello"
+
+
