6dbb0bf3eefcaf1401202603a3bafbb5ff8e0926
Hector Acosta <hacostacervantes17@mail.wou.edu>
2019-10-27 23:30:10 +0000

    Added test case of handeling GET activities list with proper list of data that will be retured.

--- test_app.py
+++ test_app.py
@@ -6,3 +6,8 @@ def test_get_activities_formats_log():
     response = app.test_client().get('/api/activities/',content_type='application/json')
     data = json.loads(response.get_data())
     assert "activity_log" in data
+
+def test_get_activities_has_data_list():
+    response = app.test_client().get('/api/activities/',content_type='application/json')
+    data = json.loads(response.get_data())
+    assert isinstance(data["activity_log"], list)
