7193da7233ab3e040bd2d6afb7478129e08f2e7d
Hector Acosta <hacostacervantes17@mail.wou.edu>
2019-10-29 03:33:27 +0000

    Added evidence of coverage report and slighly edited cov.sh

--- cov.sh
+++ cov.sh
@@ -2,6 +2,6 @@
 export M_APP_SETTINGS=$(pwd)/dev.settings
 export FLASK_ENV=test
 export FLASK_DEBUG=0
-coverage run --source "." -m py.test
+coverage run --omit dev.settings --source "." -m py.test
 
 coverage report
