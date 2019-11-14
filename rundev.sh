7b4ae1614e0b7cba9a04c9bb92cbd35905c6ef7b
Hector Acosta <hacostacervantes17@mail.wou.edu>
2019-11-14 17:22:10 +0000

    Removed files that are not needed.

--- rundev.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-export FLASK_APP=app.py
-export FLASK_ENV=development
-export M_APP_SETTINGS=$(pwd)/dev.settings
-
-flask run --host=0.0.0.0 --port $@
--- runtests.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-export M_APP_SETTINGS=$(pwd)/dev.settings
-export FLASK_ENV=test
-export FLASK_DEBUG=0
-python -m pytest -vv
