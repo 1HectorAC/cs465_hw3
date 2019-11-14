f27b6d9cbcee56a904fd1a6ce926e57a356ef7d8
Hector Acosta <hacostacervantes17@mail.wou.edu>
2019-11-14 17:39:26 +0000

    Removed cov.sh file.

--- cov.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-export M_APP_SETTINGS=$(pwd)/dev.settings
-export FLASK_ENV=test
-export FLASK_DEBUG=0
-coverage run --omit dev.settings --source "." -m py.test
-
-coverage report
